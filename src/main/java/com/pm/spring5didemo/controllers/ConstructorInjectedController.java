46b806665f77d84d71f96bbdbda8dfa4fee90944
Mizer Piotr <mizer.piotr@gmail.com>
2019-07-29 14:50:29 +0200

    Extensions of Controllers

--- src/main/java/com/pm/spring5didemo/controllers/ConstructorInjectedController.java
+++ src/main/java/com/pm/spring5didemo/controllers/ConstructorInjectedController.java
@@ -2,6 +2,7 @@ package com.pm.spring5didemo.controllers;
 
 import com.pm.spring5didemo.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,8 +10,8 @@ public class ConstructorInjectedController {
 
     private GreetingService greetingService;
 
-    @Autowired
-    public ConstructorInjectedController(GreetingService greetingService) {
+
+    public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
