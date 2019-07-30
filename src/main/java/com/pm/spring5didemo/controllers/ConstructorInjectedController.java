e4d325e9196587d5b40b2da365ab24754a9ac727
Mizer Piotr <mizer.piotr@gmail.com>
2019-07-30 08:41:53 +0200

    Finishing touch

--- src/main/java/com/pm/spring5didemo/controllers/ConstructorInjectedController.java
+++ src/main/java/com/pm/spring5didemo/controllers/ConstructorInjectedController.java
@@ -1,7 +1,6 @@
 package com.pm.spring5didemo.controllers;
 
 import com.pm.spring5didemo.services.GreetingService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
